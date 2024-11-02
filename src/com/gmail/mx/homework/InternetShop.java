e3d78cbb4e09bacaa34153e5cbb3f4bde2d3a3f2
Maksym Khomenko <mv_2010@i.ua>
2024-11-02 21:20:03 +0200

    Teacher's version

--- src/com/gmail/mx/homework/InternetShop.java
+++ src/com/gmail/mx/homework/InternetShop.java
@@ -3,23 +3,45 @@ package com.gmail.mx.homework;
 public class InternetShop {
     public static void main(String[] args) {
 
-        int order = 1 ;
-        String client = " Client: Alice";
+//                ⬇ my version of homework ⬇
+//      int order = 1 ;
+//      String client = " Client: Alice";
+//      String product = "smartphone";
+//      double price = 305.99;
+//      String address = "Moon Street, 10";
+//      System.out.println("Order № " + order + client +".");
+//      System.out.println("Product: " + product + ",");
+//      System.out.println("price EUR " + price + ".");
+//      System.out.println("Address: " + address + ".");
+//
+//      order = 2 ;
+//      client = " Client: Tom";
+//      product = "Laptop";
+//      price = 570.95;
+//      address = "Terre Street, 17";
+//      System.out.println("Order № " + order + client + "."+"\n"+"Product: " + product + "."+"\n"+"price EUR " + price + "."+"\n"+"Address " + address + ".");
+
+//               ⬇ teacher's version ⬇
+
+        byte number = 1;
+        String client = "Alice";
         String product = "smartphone";
         double price = 305.99;
         String address = "Moon Street, 10";
-        System.out.println("Order № " + order + client +".");
+        System.out.println("Order No " + number + " Client: " + client + ".");
         System.out.println("Product: " + product + ",");
         System.out.println("price EUR " + price + ".");
         System.out.println("Address: " + address + ".");
 
-        order = 2 ;
-        client = " Client: Tom";
-        product = "Laptop";
+        number++;
+        client = "Tom";
+        product = "laptop";
         price = 570.95;
-        address = "Terre Street, 17";
-        System.out.println("Order № " + order + client + "."+"\n"+"Product: " + product + "."+"\n"+"price EUR " + price + "."+"\n"+"Address " + address + ".");
-
+        address = "Terra Street, 17";
+        System.out.println("Order No " + number + " Client: " + client + ".");
+        System.out.println("Product: " + product + ",");
+        System.out.println("price EUR " + price + ".");
+        System.out.println("Address: " + address + ".");
     }
 
 
