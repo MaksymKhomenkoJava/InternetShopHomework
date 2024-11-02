96fce21c26bd1c1871ee8f2248b434e23f03c834
Maksym Khomenko <mv_2010@i.ua>
2024-11-02 21:07:00 +0200

    Initial commit

---
+++ src/com/gmail/mx/homework/InternetShop.java
@@ -0,0 +1,26 @@
+package com.gmail.mx.homework;
+
+public class InternetShop {
+    public static void main(String[] args) {
+
+        int order = 1 ;
+        String client = " Client: Alice";
+        String product = "smartphone";
+        double price = 305.99;
+        String address = "Moon Street, 10";
+        System.out.println("Order № " + order + client +".");
+        System.out.println("Product: " + product + ",");
+        System.out.println("price EUR " + price + ".");
+        System.out.println("Address: " + address + ".");
+
+        order = 2 ;
+        client = " Client: Tom";
+        product = "Laptop";
+        price = 570.95;
+        address = "Terre Street, 17";
+        System.out.println("Order № " + order + client + "."+"\n"+"Product: " + product + "."+"\n"+"price EUR " + price + "."+"\n"+"Address " + address + ".");
+
+    }
+
+
+    }
