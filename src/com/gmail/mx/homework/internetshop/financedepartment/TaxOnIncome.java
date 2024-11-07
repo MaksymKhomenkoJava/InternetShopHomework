a13aae1cc6e5de764ea49f5fb0086205c35faad5
Maksym Khomenko <mv_2010@i.ua>
2024-11-07 07:12:08 +0200

    HomeWork5

--- src/com/gmail/mx/homework/internetshop/financedepartment/TaxOnIncome.java
+++ src/com/gmail/mx/homework/internetshop/financedepartment/TaxOnIncome.java
@@ -14,17 +14,16 @@ public class TaxOnIncome {
         double maximumRate = 0.043;
         double mediumRate = 0.067;
 
-
         if (income <= 10000) {
             System.out.println("Tax rate 2.5% ");
-            System.out.printf("Tax amount EUR: %.2f",income * minimumRate);
+            System.out.printf("Tax amount EUR: %.2f\n",income * minimumRate);
         } else {
             if (income <= 25000) {
             System.out.println("Tax rate 4.3% ");
-            System.out.printf("Tax amount EUR: %.2f",income * mediumRate);
+            System.out.printf("Tax amount EUR: %.2f\n",income * mediumRate);
         } else {
             System.out.println("Tax rate 6.7% ");
-            System.out.printf("Tax amount EUR: %.2f",income * maximumRate);
+            System.out.printf("Tax amount EUR: %.2f\n",income * maximumRate);
             }
         }
         }
