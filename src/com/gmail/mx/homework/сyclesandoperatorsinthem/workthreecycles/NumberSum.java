dfba9fc0ccf7820321c5985b084037a639e0d135
Maksym Khomenko <mv_2010@i.ua>
2024-11-08 17:30:20 +0200

    NumSum1

--- src/com/gmail/mx/homework/сyclesandoperatorsinthem/workthreecycles/NumberSum.java
+++ src/com/gmail/mx/homework/сyclesandoperatorsinthem/workthreecycles/NumberSum.java
@@ -2,6 +2,12 @@ package com.gmail.mx.homework.сyclesandoperatorsinthem.workthreecycles;
 
 public class NumberSum {
     public static void main(String[] args) {
-        
+        int j = 1;
+        int k = 1;
+        for (int i = 1; i < 7; j ++, k++, i++){
+            int sum = j+i;
+            System.out.println(k + ") Num is " + i + " sum is " + sum);
+
+        }
     }
 }
