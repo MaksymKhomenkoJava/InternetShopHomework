6b420d23ade00fbdc69ef147a53ceafa810cc2e3
Maksym Khomenko <mv_2010@i.ua>
2024-11-10 12:36:20 +0200

    NumSumred2

--- src/com/gmail/mx/homework/сyclesandoperatorsinthem/workthreecycles/NumberSum.java
+++ src/com/gmail/mx/homework/сyclesandoperatorsinthem/workthreecycles/NumberSum.java
@@ -4,10 +4,12 @@ public class NumberSum {
     public static void main(String[] args) {
         int j = 1;
         int k = 1;
-        for (int i = 1; i < 7; j ++, k++, i++){
-            int sum = j+i;
-            System.out.println(k + ") Num is " + i + " sum is " + sum);
+
+        for (int i = 1; i < 7; k++, i++, j += i) {
+
+            System.out.println(k + ") Num is " + i + " sum is " + j);
 
         }
+        System.out.println("Sum of numbers " + " " + j);
     }
 }
