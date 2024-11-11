ea777ffbb9183eb016f432256cc6fffc45d760f3
Maksym Khomenko <mv_2010@i.ua>
2024-11-11 17:50:42 +0200

    NumSumred3

--- src/com/gmail/mx/homework/сyclesandoperatorsinthem/workthreecycles/NumberSum.java
+++ src/com/gmail/mx/homework/сyclesandoperatorsinthem/workthreecycles/NumberSum.java
@@ -1,15 +1,42 @@
 package com.gmail.mx.homework.сyclesandoperatorsinthem.workthreecycles;
 
+import java.util.Scanner;
+
 public class NumberSum {
     public static void main(String[] args) {
-        int j = 1;
-        int k = 1;
+        System.out.println("Enter the num: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int sum = 0;
 
-        for (int i = 1; i < 7; k++, i++, j += i) {
+//        for (int i = 1; i <= num; i++) {
+//            sum += i;
+//            System.out.println(i + ") Num is " + i + " sum is " + sum);
+//        }
 
-            System.out.println(k + ") Num is " + i + " sum is " + j);
-
+        int i = 1;
+        while (i <= num) {
+            sum += i;
+            System.out.println(i + ") Num is " + i + " sum is " + sum);
+            i++;
         }
-        System.out.println("Sum of numbers " + " " + j);
+        System.out.println("..............................");
+        System.out.println("Sum of numbers is " + sum);
+
+
+//        if (num == 0) {
+//           return;
+//        }
+//        int i = 1;
+//        do {
+//            sum += i;
+//            System.out.println(i + ") Num is " + i + " sum is " + sum);
+//
+//            i++;
+//        } while (i <= num);
+//        System.out.println("..............................");
+//        System.out.println("Sum of numbers is " + sum);
     }
 }
+
+//}
