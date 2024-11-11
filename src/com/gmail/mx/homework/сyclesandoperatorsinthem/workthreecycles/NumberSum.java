5230112fdd35c6f5c5936cebd497e365f5735e11
Maksym Khomenko <mv_2010@i.ua>
2024-11-11 19:09:07 +0200

    NumSumred4

--- src/com/gmail/mx/homework/сyclesandoperatorsinthem/workthreecycles/NumberSum.java
+++ src/com/gmail/mx/homework/сyclesandoperatorsinthem/workthreecycles/NumberSum.java
@@ -4,17 +4,25 @@ import java.util.Scanner;
 
 public class NumberSum {
     public static void main(String[] args) {
+
+
         System.out.println("Enter the num: ");
         Scanner scanner = new Scanner(System.in);
+
         int num = scanner.nextInt();
         int sum = 0;
 
-//        for (int i = 1; i <= num; i++) {
-//            sum += i;
-//            System.out.println(i + ") Num is " + i + " sum is " + sum);
-//        }
+        for (int i = 1; i <= num; i++) {
+            sum += i;
+            System.out.println(i + ") Num is " + i + " sum is " + sum);
+
+        }
+        System.out.println("..............................");
+        System.out.println("Sum of numbers is " + sum);
+        System.out.println();
 
         int i = 1;
+        sum = 0;
         while (i <= num) {
             sum += i;
             System.out.println(i + ") Num is " + i + " sum is " + sum);
@@ -22,21 +30,23 @@ public class NumberSum {
         }
         System.out.println("..............................");
         System.out.println("Sum of numbers is " + sum);
+        System.out.println();
 
+        if (num == 0) {
+            return;
+        }
+ //       int i = 1;
+        do {
+            sum =0;
+            sum += i;
+            System.out.println(i + ") Num is " + i + " sum is " + sum);
 
-//        if (num == 0) {
-//           return;
-//        }
-//        int i = 1;
-//        do {
-//            sum += i;
-//            System.out.println(i + ") Num is " + i + " sum is " + sum);
-//
-//            i++;
-//        } while (i <= num);
-//        System.out.println("..............................");
-//        System.out.println("Sum of numbers is " + sum);
+            i++;
+        } while (i <= num);
+        System.out.println("..............................");
+        System.out.println("Sum of numbers is " + sum);
     }
 }
 
-//}
+
+
