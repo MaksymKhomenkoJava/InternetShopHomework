18b0888af87a4040a6f3d6df1414c3225969b0b0
Maksym Khomenko <mv_2010@i.ua>
2024-11-13 18:50:43 +0200

    ArrayElementsRed3

--- src/com/gmail/mx/homework/allarray/workarray/WorkWithArray.java
+++ src/com/gmail/mx/homework/allarray/workarray/WorkWithArray.java
@@ -5,32 +5,31 @@ import java.util.Arrays;
 public class WorkWithArray {
     public static void main(String[] args) {
 
-        int a = (int) (Math.random() * 200 - 100);
-
-        System.out.println("Number : " + a);
-
-
-        int[] numbers = {1, -2, -12, 3, 4, -5, 60, 7, 81, 9, -10};
-        for (int current : numbers) {
-            System.out.println("Elements:" + current);
-        }
-
-        System.out.println("Elements array numbers: ");
-        int sum = 0;
+        System.out.println("Elements array: ");
+        int[] numbers = new int[20];
         for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
-            System.out.println("Elements[" + i + "]: " + numbers[i]);
-
+            numbers[i] = (int) (Math.random() * 200 - 100);
+            System.out.println(" index [" + i + "]" + "  " + numbers[i]);
         }
-        System.out.println("Sum elements array: " + sum);
-
         int max = numbers[0];
+        int index = 0;
         for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] > max) {
                 max = numbers[i];
+                index = i;
             }
         }
-        System.out.println("Max number array: " + max);
+        System.out.println("Max number array: " + max + "[" + index + "]");
+
+        int min = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+                index = i;
+            }
+        }
+        System.out.println("Max number array: " + min + "[" + index + "]");
+
 
     }
 }
