e5d93c7723287689e2192eb8c88867b1781daf40
Maksym Khomenko <mv_2010@i.ua>
2024-11-14 17:30:04 +0200

    ArrayElementsRed8

--- src/com/gmail/mx/homework/allarray/workarray/WorkWithArray.java
+++ src/com/gmail/mx/homework/allarray/workarray/WorkWithArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class WorkWithArray {
     public static void main(String[] args) {
-
+// масив з випадкових цылих чисел
         System.out.println("1. Elements array: ");
         int[] numbers = new int[20];
         for (int i = 0; i < numbers.length; i++) {
@@ -12,7 +12,7 @@ public class WorkWithArray {
             System.out.println("index [" + i + "]" + " number " + numbers[i]);
         }
         System.out.println(" ");
-
+// мынымальний та максимальний елемент масиву
         int max = numbers[0];
         int min = numbers[0];
         int indexmax = 0;
@@ -30,7 +30,7 @@ public class WorkWithArray {
         System.out.println("2. Max array element: " + max + "[" + indexmax + "]");
         System.out.println("3. Min array element: " + min + "[" + indexmin + "]");
         System.out.println(" ");
-
+// парні та непарні числа
         int a = 0;
         int b = 0;
         for (int i = 0; i < numbers.length; i++) {
@@ -44,7 +44,7 @@ public class WorkWithArray {
         System.out.println("4. Even array element: " + a);
         System.out.println("5. Odd array element: " + b);
         System.out.println(" ");
-
+// сума від'ємних чисел
         int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] < 0) {
@@ -53,25 +53,32 @@ public class WorkWithArray {
         }
         System.out.println("6. Sum of negative array element: " + sum);
         System.out.println(" ");
+// перше негативне, сума елементів після першої негативної, сума всіх улементів
+        a = 0;
+        b = 0;
+        while (a >= 0 && a < 20) {
+            if (numbers[a] < 0) {
+                a = numbers[a];
+            }
+            a++;
+            b++;
+        }
+        System.out.println("first negative number:" + (a - 1));
 
-////        sum = 0;
-//        int j = 0;
-//        while (j < 1) {
-//            int i = 0;
-//            if (numbers[i] < 0) {
-//                j = numbers[i];
-//                break;
-//            }
-//            i = i + 1;
-//
-//            System.out.println("first negative number:" + j);
-//
-//        }
+        for (int i = b + 1; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println("Sum of numbers after the first negative:" + sum);
 
-
-//        for (int i = a + 1; i < numbers.length; i++) {
-//            sum += numbers[i];
-//        }
-//        System.out.println("Sum of numbers after the first negative:" + sum);
+        int sumarray = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sumarray += numbers[i];
+        }
+        System.out.println("Sumarray all element: " + sumarray);
     }
+
+
 }
+
+
+
