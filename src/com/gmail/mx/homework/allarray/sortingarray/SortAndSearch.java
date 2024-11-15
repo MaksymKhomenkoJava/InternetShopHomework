3717732f87fe0550897836c638db80b55e13aa74
Maksym Khomenko <mv_2010@i.ua>
2024-11-16 00:10:26 +0200

    WorkWhithArrayCorrect1

--- src/com/gmail/mx/homework/allarray/sortingarray/SortAndSearch.java
+++ src/com/gmail/mx/homework/allarray/sortingarray/SortAndSearch.java
@@ -1,19 +1,44 @@
 package com.gmail.mx.homework.allarray.sortingarray;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SortAndSearch {
-       public static void main(String[] args){
+    public static void main(String[] args) {
 
-           System.out.println("Enter array elements: ");
-           Scanner scanner = new Scanner(System.in);
-           int numeric = scanner.nextInt();
-           int[] numbers = new int[numeric];
+        System.out.println("Enter array elements: ");
 
-           for (int i = 0; i < numbers.length; i++) {
-               numbers[i] = (int) (Math.random() * 100);
-               System.out.println("index [" + i + "]" + " number " + numbers[i]);
-           }
-           System.out.println(" ");
-    }
+        Scanner scanner = new Scanner(System.in);
+        int numeric = scanner.nextInt();
+        int[] array = new int[numeric];
+
+        System.out.print("Array: [");
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+            System.out.print(array[i] + " ");
+        }
+        System.out.print(" ]");
+        System.out.println();
+
+        int n = array.length;
+        int minIndex = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int k = i + 1; k < n; k++) {
+                minIndex = i;
+                if (array[k] < array[i]) {
+                    minIndex = k;
+                }
+                if (minIndex != i) {
+                    int a = array[i];
+                    array[i] = array[k];
+                    array[k] = a;
+                    System.out.print(" "+array[k]+" ");
+                }
+
+
+            }
+
+        }
+            }
 }
--- src/com/gmail/mx/homework/allarray/workarray/WorkWithArray.java
+++ src/com/gmail/mx/homework/allarray/workarray/WorkWithArray.java
@@ -13,50 +13,50 @@ public class WorkWithArray {
         }
         System.out.println(" ");
 
-        int max = numbers[0];
-        int min = numbers[0];
-        int indexmax = 0;
-        int indexmin = 0;
+        int maxElement = numbers[0];
+        int minElement = numbers[0];
+        int indexMax = 0;
+        int indexMin = 0;
         for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] > max) {
-                max = numbers[i];
-                indexmax = i;
+            if (numbers[i] > maxElement) {
+                maxElement = numbers[i];
+                indexMax = i;
             }
-            if (numbers[i] < min) {
-                min = numbers[i];
-                indexmin = i;
+            else if (numbers[i] < minElement) {
+                minElement = numbers[i];
+                indexMin = i;
             }
         }
-        System.out.println("2. Max array element: " + max + "[" + indexmax + "]");
-        System.out.println("3. Min array element: " + min + "[" + indexmin + "]");
+        System.out.println("2. Max array element: " + maxElement + "[" + indexMax + "]");
+        System.out.println("3. Min array element: " + minElement + "[" + indexMin + "]");
+        System.out.println(" ");
+
+        int evenElement = 0;
+        int oddElement = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                evenElement++;
+            }
+            if (numbers[i] % 2 != 0) {
+                oddElement++;
+            }
+        }
+        System.out.println("4. Even array element: " + evenElement);
+        System.out.println("5. Odd array element: " + oddElement);
+        System.out.println(" ");
+
+        int sumNegative = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < 0) {
+                sumNegative += numbers[i];
+            }
+        }
+        System.out.println("6. Sum of negative array element: " + sumNegative);
         System.out.println(" ");
 
         int a = 0;
         int b = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 == 0) {
-                a++;
-            }
-            if (numbers[i] % 2 != 0) {
-                b++;
-            }
-        }
-        System.out.println("4. Even array element: " + a);
-        System.out.println("5. Odd array element: " + b);
-        System.out.println(" ");
-
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] < 0) {
-                sum += numbers[i];
-            }
-        }
-        System.out.println("6. Sum of negative array element: " + sum);
-        System.out.println(" ");
-
-        a = 0;
-        b = 0;
-        while (a >= 0 && a < 20) {
+        while (a >= 0 && a < numbers.length) {
             if (numbers[a] < 0) {
                 a = numbers[a];
             }
@@ -64,21 +64,21 @@ public class WorkWithArray {
             b++;
         }
         System.out.println("7. first negative number:" + (a - 1));
-        int sum1 = 0;
+        int sumAfterFirst = 0;
         for (int i = b ; i < numbers.length; i++) {
-            sum1 += numbers[i];
+            sumAfterFirst += numbers[i];
         }
-        System.out.println("8. Sum of numbers after the first negative:" + sum1);
+        System.out.println("8. Sum of numbers after the first negative: " + sumAfterFirst);
 
-        int average = sum1/(numbers.length-b);
+        int average = sumAfterFirst/(numbers.length-b);
 
         System.out.println("9. Arithmetic average : " + average);
 
-        int sumarray = 0;
+        int sumArray = 0;
         for (int i = 0; i < numbers.length; i++) {
-            sumarray += numbers[i];
+            sumArray += numbers[i];
         }
-        System.out.println("10. Sum array all element: " + sumarray);
+        System.out.println("10. Sum array all element: " + sumArray);
     }
 
 
