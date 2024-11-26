6e1004435078f62c5989e3acc0cb378a9bd238dc
Maksym Khomenko <mv_2010@i.ua>
2024-11-26 21:55:15 +0200

    MethodsPrograms2

--- src/com/gmail/mx/homework/methodswork/MethodsPrograms.java
+++ src/com/gmail/mx/homework/methodswork/MethodsPrograms.java
@@ -17,7 +17,13 @@ public class MethodsPrograms {
         double height = scanner.nextDouble();
         System.out.println("Volume cylinder with radius " + radius + " and height " + height + " equals " + calculateCylinderVolume(radius, height));
 
+        System.out.println("Enter array elements: ");
+        int number = scanner.nextInt();
+        System.out.println("Sum array elements: " + sumArray(number));
 
+
+//        String[] lines = {"=PRINT METHOD="};
+//        System.out.println("=" + reversString(lines) + "=");
     }
 
     public static int pow(int value) {
@@ -25,6 +31,37 @@ public class MethodsPrograms {
     }
 
     public static double calculateCylinderVolume(double radius, double height) {
-        return 3.14*radius*radius*height;
+        return 3.14 * radius * radius * height;
     }
+
+    public static int sumArray(int number) {
+        int[] array = new int[number];
+        System.out.print("Array: ");
+        System.out.println(" ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+            System.out.print(array[i] + " ");
+        }
+        System.out.println(" ");
+        int sumArray = 0;
+        for (int i = 0; i < array.length; i++) {
+            sumArray += array[i];
+        }
+        return sumArray;
+    }
+
+
+//    public static String reversString(String[] lines) {
+////        lines[i] = lines.toString();
+//        StringBuilder sb = new StringBuilder(lines);
+//        reversedS
+//        String result = "";
+//        for (int i = 0; i < lines.length-1; i--) {
+//            result = result + lines[i];
+//        }
+//        return result;
+//    }
+
 }
+
+
