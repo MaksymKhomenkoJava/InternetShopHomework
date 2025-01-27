1ac506a6015d4871696353de8a604e397c92551e
Maksym Khomenko <mv_2010@i.ua>
2025-01-27 21:37:09 +0200

    MVN Project

--- src/main/java/com/gmail/mx/homework/oopworks/enumwork/Advisor.java
+++ src/main/java/com/gmail/mx/homework/oopworks/enumwork/Advisor.java
@@ -15,13 +15,11 @@ public class Advisor implements AdviseMethod {
     public void advise(Day day) {
         switch (day) {
             case MONDAY, TUESDAY, WEDNESDAY, THURSDAY ->
-                System.out.println("Be productive at work! Set clear goals and manage your time effectively");
-            case FRIDAY ->
-                    System.out.println("Happy Friday! Plan your weekend and relax");
-            case  SATURDAY, SUNDAY ->
+                    System.out.println("Be productive at work! Set clear goals and manage your time effectively");
+            case FRIDAY -> System.out.println("Happy Friday! Plan your weekend and relax");
+            case SATURDAY, SUNDAY ->
                     System.out.println("It's the weekend! Visit a park, museum, or enjoy time with family and friends");
-            default ->
-                    System.out.println("Invalid day");
+            default -> System.out.println("Invalid day");
         }
     }
 }
