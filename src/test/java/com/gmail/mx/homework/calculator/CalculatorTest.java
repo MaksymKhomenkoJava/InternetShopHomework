56d435532d38c42c4853f89aa1a166645654517b
Maksym Khomenko <mv_2010@i.ua>
2025-02-04 23:08:23 +0200

    Junit2

--- src/test/java/com/gmail/mx/homework/calculator/CalculatorTest.java
+++ src/test/java/com/gmail/mx/homework/calculator/CalculatorTest.java
@@ -1,13 +1,67 @@
 package com.gmail.mx.homework.calculator;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+    //    @Test
+//    void zero() {
+//        // give
+//        Calculator calculator = new Calculator();
+//
+//        //when
+////        calculator.factorial(0);
+//        long result = calculator.factorial(0);
+//
+//        //then
+//        Assertions.assertNotNull(calculator);
+//        Assertions.assertEquals(1, result);
+//        System.out.println("0! ==" + calculator.factorial(0));
+//        ;
+//    }
+//
+//    @Test
+//    void one() {
+//        // give
+//        Calculator calculator = new Calculator();
+//
+//        //when
+////        calculator.factorial(1);
+//        long result = calculator.factorial(1);
+//
+//        //then
+//        Assertions.assertNotNull(calculator);
+//        Assertions.assertEquals(1, result);
+//        System.out.println("1! ==" + calculator.factorial(3));
+//        ;
+//    }
+//
+//    @Test
+//    void three() {
+//        // give
+//        Calculator calculator = new Calculator();
+//
+//        //when
+////        calculator.factorial(1);
+//        long result = calculator.factorial(3);
+//
+//        //then
+//        Assertions.assertNotNull(calculator);
+//        Assertions.assertEquals(6, result);
+//        System.out.println("3! ==" + calculator.factorial(6));
+//    }
+private Calculator calculator;
+    @BeforeEach
+    void setUp(){
+        calculator = new Calculator();
+        System.out.println("beforeeach");
+    }
+
     @Test
     void zero() {
         // give
-        Calculator calculator = new Calculator();
+        System.out.println("zero");
 
         //when
 //        calculator.factorial(0);
@@ -16,14 +70,14 @@ public class CalculatorTest {
         //then
         Assertions.assertNotNull(calculator);
         Assertions.assertEquals(1, result);
-        System.out.println("0! ==" + calculator.factorial(0));
-        ;
+
+
     }
 
     @Test
     void one() {
         // give
-        Calculator calculator = new Calculator();
+        System.out.println("one");
 
         //when
 //        calculator.factorial(1);
@@ -32,14 +86,12 @@ public class CalculatorTest {
         //then
         Assertions.assertNotNull(calculator);
         Assertions.assertEquals(1, result);
-        System.out.println("1! ==" + calculator.factorial(3));
-        ;
     }
 
     @Test
     void three() {
         // give
-        Calculator calculator = new Calculator();
+
 
         //when
 //        calculator.factorial(1);
@@ -48,6 +100,6 @@ public class CalculatorTest {
         //then
         Assertions.assertNotNull(calculator);
         Assertions.assertEquals(6, result);
-        System.out.println("3! ==" + calculator.factorial(6));
+        System.out.println("three");
     }
 }
